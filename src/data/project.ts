8deb99332986b719b46e2a55c59464c1706434a0
beargreenholtz <beargreenholtz@gmail.com>
2023-04-23 19:26:37 +0300

    code refactor

---
+++ src/data/project.ts
@@ -0,0 +1,7 @@
+export const project = [
+	{ link: 'https://spirng-protfolio-frontend-production.vercel.app/', description: '2022' , image:"https://i.imgur.com/IsZNmSv.jpg", title:"Spring Portfolio"},
+    { link: 'https://exlint.io/', description: '2022' , image:"https://i.imgur.com/L0Ce85P.jpg", title:"Exlint Landing Page"},
+    { link: 'https://vinyl-depository-landing-page.vercel.app/', description: '2021' , image:"https://i.imgur.com/nZ6qXjs.png", title:"Vinyl Depository"},
+	{ link: 'https://seashell-app-3ar2i.ondigitalocean.app/', description: '2023' , image:"https://i.imgur.com/OT5EMb5.png", title:"Aviv Shiloh Portfolio"},
+	{ link: 'https://royshiloh.com/', description: '2023' , image:"https://i.imgur.com/34AlHtq.jpg", title:"Roy Portfolio"},
+];
