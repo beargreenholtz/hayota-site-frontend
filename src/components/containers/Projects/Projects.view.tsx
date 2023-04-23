722a2e20124224e0e3bf76a94e0b2cacce9d85c7
beargreenholtz <beargreenholtz@gmail.com>
2023-04-23 19:16:41 +0300

    fix mobile project

--- src/components/containers/Projects/Projects.view.tsx
+++ src/components/containers/Projects/Projects.view.tsx
@@ -23,7 +23,7 @@ const ProjectsView: React.FC<IProps> = (
 
         <ScrollContainer
           hideScrollbars
-          className={classes['projecstContainer']}
+          className={classes['projecstContainerWeb']}
         >
           <MProjectCard
             projectLink="https://spirng-protfolio-frontend-production.vercel.app/"
@@ -60,8 +60,46 @@ const ProjectsView: React.FC<IProps> = (
             projectImageLocation="https://i.imgur.com/34AlHtq.jpg"
             projectTitle="Roy Portfolio"
           />
-
         </ScrollContainer>
+
+        <div className={classes['projecstContainerMobile']}>
+        <MProjectCard
+            projectLink="https://spirng-protfolio-frontend-production.vercel.app/"
+            projectDesc="2022"
+            projectImageLocation="https://i.imgur.com/IsZNmSv.jpg"
+            projectTitle="Spring Portfolio"
+          />
+
+          <MProjectCard
+            projectLink="https://exlint.io/"
+            projectDesc="2022"
+            projectImageLocation="https://i.imgur.com/L0Ce85P.jpg"
+            projectTitle="Exlint Landing Page"
+          />
+
+         <MProjectCard
+            projectLink="https://vinyl-depository-landing-page.vercel.app/"
+            projectDesc="2021"
+            projectImageLocation="https://i.imgur.com/nZ6qXjs.png"
+            projectTitle="Vinyl Depository"
+          />
+          
+          <MProjectCard
+            projectLink="https://seashell-app-3ar2i.ondigitalocean.app/"
+            projectDesc="2023"
+            projectImageLocation="https://i.imgur.com/OT5EMb5.png"
+            projectTitle="Aviv Shiloh Portfolio"
+          />
+
+
+          <MProjectCard
+            projectLink="https://royshiloh.com/"
+            projectDesc="2023"
+            projectImageLocation="https://i.imgur.com/34AlHtq.jpg"
+            projectTitle="Roy Portfolio"
+          />
+
+        </div>
       </section>
     </>
   );
