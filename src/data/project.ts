64059f2c66d09d3c7e1c8679d5e3e13aab230696
beargreenholtz <114157462+beargreenholtz@users.noreply.github.com>
2023-04-27 23:27:45 +0300

    Update project.ts

--- src/data/project.ts
+++ src/data/project.ts
@@ -2,7 +2,7 @@ export const project = [
 	{ link: 'https://spirng-protfolio-frontend-production.vercel.app/', description: '2022' , image:"https://i.imgur.com/IsZNmSv.jpg", title:"Spring Portfolio"},
     { link: 'https://exlint.io/', description: '2022' , image:"https://i.imgur.com/L0Ce85P.jpg", title:"Exlint Landing Page"},
     { link: 'https://vinyl-depository-landing-page.vercel.app/', description: '2021' , image:"https://i.imgur.com/AOf4FMi.jpg", title:"Vinyl Depository"},
-	{ link: 'https://seashell-app-3ar2i.ondigitalocean.app/', description: '2023' , image:"https://i.imgur.com/OT5EMb5.png", title:"Aviv Shiloh Portfolio"},
+	{ link: 'https://aviv-shiloh-portfolio-frontend-95ypt.ondigitalocean.app/', description: '2023' , image:"https://i.imgur.com/OT5EMb5.png", title:"Aviv Shiloh Portfolio"},
 	{ link: 'https://royshiloh.com/', description: '2023' , image:"https://i.imgur.com/34AlHtq.jpg", title:"Roy Portfolio"},
 	{ link: 'https://impatient-something-667266.framer.app/', description: '2023' , image:"https://i.imgur.com/MCHjkY3.jpg", title:"Nobodies"},
 
