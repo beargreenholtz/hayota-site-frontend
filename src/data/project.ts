d2cd04abb53962e745b6481e1b541c89354f8c37
beargreenholtz <beargreenholtz@gmail.com>
2023-05-02 22:03:08 +0300

    link change

--- src/data/project.ts
+++ src/data/project.ts
@@ -1,9 +1,9 @@
 export const project = [
-	{ link: 'https://spirng-protfolio-frontend-production.vercel.app/', description: '2022' , image:"https://i.imgur.com/IsZNmSv.jpg", title:"Spring Portfolio"},
+	{ link: 'https://frontend-hudws.ondigitalocean.app/', description: '2022' , image:"https://i.imgur.com/IsZNmSv.jpg", title:"Spring Portfolio"},
     { link: 'https://exlint.io/', description: '2022' , image:"https://i.imgur.com/L0Ce85P.jpg", title:"Exlint Landing Page"},
     { link: 'https://vinyl-depository-landing-page.vercel.app/', description: '2021' , image:"https://i.imgur.com/AOf4FMi.jpg", title:"Vinyl Depository"},
 	{ link: 'https://aviv-shiloh-portfolio-frontend-95ypt.ondigitalocean.app/', description: '2023' , image:"https://i.imgur.com/OT5EMb5.png", title:"Aviv Shiloh Portfolio"},
 	{ link: 'https://royshiloh.com/', description: '2023' , image:"https://i.imgur.com/34AlHtq.jpg", title:"Roy Portfolio"},
-	{ link: 'https://impatient-something-667266.framer.app/', description: '2023' , image:"https://i.imgur.com/MCHjkY3.jpg", title:"Nobodies"},
+	{ link: 'https://www.nobodi.es/', description: '2023' , image:"https://i.imgur.com/MCHjkY3.jpg", title:"Nobodies"},
 
 ];
